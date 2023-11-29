fc9a9527d9eaf2ecab4bfcb3e1c8394558de35b3
DevLudo <60669196+DevLudo@users.noreply.github.com>
2023-11-29 17:49:47 +0100

    feat: add role model

---
+++ src/app/models/role.ts
@@ -0,0 +1,9 @@
+export interface Role {
+    id?: number,
+    name: string,
+    description: string,
+    privilegeLevel: number,
+    status: boolean,
+    modificationHistory: string,
+    createdAt: string
+}
