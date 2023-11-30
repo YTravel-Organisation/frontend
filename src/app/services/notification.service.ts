e47f7862f79c4a9bc83ee2acc59dcccd62ddb749
DevLudo <60669196+DevLudo@users.noreply.github.com>
2023-11-30 10:16:06 +0100

    feat: add notification service

---
+++ src/app/services/notification.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { INotification } from '../models/notification';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  private apiUrl = `${environment.apiUrl}/notifications`;
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<INotification[]> {
+    return this.http.get<INotification[]>(this.apiUrl);
+  }
+
+  createNotification(notificationData: INotification): Observable<INotification> {
+    return this.http.post<INotification>(this.apiUrl, notificationData);
+  }
+
+  getNotificationById(id: number): Observable<INotification> {
+    return this.http.get<INotification>(`${this.apiUrl}/${id}`);
+  }
+
+  updateNotificationById(id: number, notificationData: INotification): Observable<INotification> {
+    return this.http.put<INotification>(`${this.apiUrl}/${id}`, notificationData);
+  }
+
+  deleteNotificationById(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+}
