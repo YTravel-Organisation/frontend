c85d68b9dc89e461305a92fb15c29561d1a50a0d
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-02-28 14:03:45 +0100

    feat: add color constants

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+const COLORS = {
+    FOLLY: "#E6195D",
+    FRENCH_FUCHSIA: "#DC579B",
+    PURPLE: "#7D428B",
+    RUSSIAN_VIOLET: "#2E1E3E",
+    MAJORELLE_BLUE: "#3F3E92",
+    WHITE: "#FFFFFF",
+    BLACK: "#000000",
+}
